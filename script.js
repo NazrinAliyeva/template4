083d1eb95593c506504e90fa282ea1fd43e2141e
NazrinAliyeva <nazrinaliyeva2008@gmail.com>
2024-02-10 13:24:02 +0400

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+
+const sidebarBtn = document.querySelector('.sidebar__btn');
+const closeBtn = document.querySelector('.close__btn');
+const sidebar = document.querySelector('.side__menu');
+const sideWrapper = document.querySelector('.side-wrapper');
+const input= document.querySelector('.item')
+const btn=document.querySelector('.submit-btn')
+sidebarBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    sidebar.classList.add('active');
+    sideWrapper.classList.add('side')
+    
+    sideWrapper.innerHTML =`
+    <li class="nav-item">Home</li>
+    <li class="nav-item">About Us</li>
+    <li class="nav-item">Planters</li>
+    <li class="nav-item">Contact</li>
+    <li class="nav-item"><button class="btn-transparent">Call Us</button></li>
+    `
+    sidebar.appendChild(side);
+
+})
+
+closeBtn.addEventListener('click', function(e){
+    e.preventDefault();
+
+
+
+
+    sidebar.classList.remove('active'); 
+
+})
+
+
+
+
 const cardWrapper=document.querySelector('.card-wrapper')
 const products=[
     {
@@ -69,6 +105,7 @@ products.forEach((product) => {
     card.innerHTML = `
 <div class="wrapper">                <div>
 <img src="./img/${product.image}" alt="" class="card-img">
+<img src="./img/heart.svg" alt="" class="heart-icon">
 </div>
 <div class="card-info-wrapper">
 <h1 class="card-title">${product.title}</h1>
@@ -81,4 +118,16 @@ products.forEach((product) => {
 </div>
     `
     cardWrapper.appendChild(card);
+})
+// const icon=document.querySelectorAll('.heart-icon')
+// icon.addEventListener('click',function(){
+// icon.style.display='none'
+// const blackHeart = document.createElement('div');
+// blackHeart.classList.add('black-heart')
+// blackHeart.innerHTML=`<img src="./img/black-heart.svg" alt="">`
+// card.appendChild(blackHeart)
+// })
+btn.addEventListener('click',function(e){
+    e.preventDefault();
+input.value=''
 })
